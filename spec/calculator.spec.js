094106e1d0873e18aa0092585c958cc4573f3172
simplon <rudy__10@hotmail.fr>
2017-01-20 15:34:03 +0100

    ajout des test

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -6,4 +6,19 @@ describe("StringCalculator execution", function () {
         expect(StringCalculator(null)).toBe(0);
     });
 
+	it("should return 0 when input is null", function () {
+        expect(StringCalculator("")).toBe(0);
+    });
+    it("should return 0 when input is null", function () {
+        expect(StringCalculator(0)).toBe(0);
+    });
+    it("should return 0 when input is null", function () {
+        expect(StringCalculator(1)).toBe(1);
+    });
+
+    it("should return 0 when input is null", function () {
+        expect(StringCalculator("1 + 2")).toBe(3);
+    });
+
+
 });
